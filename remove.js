5ab0466acfd81f3ac96afa32fa93e0ad9dcf88ca
David Almaraz <david.almaraz@theksquare.com>
2021-04-19 17:47:30 -0500

    changes2

--- remove.js
+++ remove.js
@@ -1,9 +1,9 @@
-const fs = require('fs').promises;
+const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 })
 
 var filesRemoved = 0;
@@ -11,62 +11,61 @@ var directoriesRemoved = 0;
 var pathsFiles = [];
 const directory = process.argv[2];
 
-(async function removeDirectory(directory) {
-  try {
-    const files = await inspectDirectory(directory);
-    if (files.length > 0) {
-      const conf = await confirm(directory);
-      if (conf) {
-        await remove(files);
-      }
-    } else {
-      await remove(directory);
+(function removeDirectory(directory) {
+    try {
+        const files = inspectDirectory(directory);
+        if (files.length > 1) {
+            rl.question(`Directory ${directory} is not empty. Do you want to delete all content? Yes/No: `, (answer) => {
+                if (answer === 'Yes' || answer === 'yes') {
+                    removeAll(files);
+                    finishProcess();
+                } else {
+                    console.log("Remove operation canceled")
+                    finishProcess();
+                }
+            });
+        } else {
+            fs.rmdirSync(directory);
+            directoriesRemoved++
+            finishProcess();
+        }
+    } catch (error) {
+        console.log(error.message);
+        finishProcess();
     }
-  } catch (error) {
-    console.log(error);
-  }
-  console.log(`You removed: ${directoriesRemoved} directories, AND ${filesRemoved} files)`);
 })(directory);
 
-async function remove(files) {
-  for (file of files) {
-    const dirStatus = await fs.lstat(file);
-    if (dirStatus.isDirectory()) {
-      await fs.rmdir(file);
-      directoriesRemoved++;
-    } else {
-      await fs.rm(file)
-      filesRemoved++
+function inspectDirectory(directory) {
+    const files = fs.readdirSync(directory);
+    if (files.length > 0) {
+        files.forEach((element) => {
+            const file = path.join(directory, element);
+            const fileStatus = fs.statSync(file);
+            if (fileStatus.isDirectory()) {
+                inspectDirectory(file);
+            } else {
+                pathsFiles.push(file);
+            }
+        });
     }
-  }
+    pathsFiles.push(directory);
+    return pathsFiles;
 }
 
-async function inspectDirectory(directory) {
-  const files = await fs.readdir(directory);
-  if (files.length > 0) {
-      for(const element of files) {
-      const file = path.join(directory, element);
-      const fileStatus = await fs.stat(file);
-      if (fileStatus.isDirectory()) {
-        await inspectDirectory(file);
-      } else {
-        pathsFiles.push(file);
-      }
-    }
-  }
-  pathsFiles.push(directory);
-  return pathsFiles;
+function removeAll(files) {
+    files.forEach((file) => {
+        const dirStatus = fs.lstatSync(file);
+        if (dirStatus.isDirectory()) {
+            fs.rmdirSync(file);
+            directoriesRemoved++;
+        } else {
+            fs.unlinkSync(file)
+            filesRemoved++;
+        }
+    });
 }
 
-function confirm(directory) {
-  return new Promise((resolve, reject) => {
-    rl.question(`Directory ${directory} is not empty. Do you want to delete all content? Yes/No: `, (answer) => {
-      if (answer === 'Yes' || answer === 'yes') {
-        rl.close();
-        resolve(true);
-      } else {
-        reject("Remove operation canceled");
-      }
-    })
-  });
+function finishProcess() {
+    console.log(`You removed: ${directoriesRemoved} directories, AND ${filesRemoved} files)`);
+    process.exit();
 }
